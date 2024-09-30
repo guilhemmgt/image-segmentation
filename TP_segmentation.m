19eed6e1bfc83fc9b9e5bdac619edcf2aa5171e7
Guilhem Mangenot <guilhem.mangenot@gmail.com>
2024-09-30 11:57:20 +0200

    init

---
+++ TP_segmentation.m
@@ -0,0 +1,53 @@
+clear;
+close all;
+% Nombre d'images utilisees
+nb_images = 36; 
+
+% chargement des images
+for i = 1:nb_images
+    if i<=10
+        nom = sprintf('images/viff.00%d.ppm',i-1);
+    else
+        nom = sprintf('images/viff.0%d.ppm',i-1);
+    end;
+    % im est une matrice de dimension 4 qui contient 
+    % l'ensemble des images couleur de taille : nb_lignes x nb_colonnes x nb_canaux 
+    % im est donc de dimension nb_lignes x nb_colonnes x nb_canaux x nb_images
+    im(:,:,:,i) = imread(nom); 
+end;
+
+% Affichage d'exemples d'images
+figure; 
+subplot(2,2,1); imshow(im(:,:,:,1)); title('Image 1');
+subplot(2,2,2); imshow(im(:,:,:,9)); title('Image 9');
+subplot(2,2,3); imshow(im(:,:,:,17)); title('Image 17');
+subplot(2,2,4); imshow(im(:,:,:,25)); title('Image 25');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% A COMPLETER                                             %
+% Calculs des superpixels                                 % 
+% Conseil : afficher les germes + les régions             %
+% à chaque étape / à chaque itération                     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% ........................................................%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% A COMPLETER                                             %
+% Binarisation de l'image à partir des superpixels        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% ........................................................%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% A DECOMMENTER ET COMPLETER                              %
+% quand vous aurez les images segmentées                  %
+% Affichage des masques associes                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% figure;
+% subplot(2,2,1); ... ; title('Masque image 1');
+% subplot(2,2,2); ... ; title('Masque image 9');
+% subplot(2,2,3); ... ; title('Masque image 17');
+% subplot(2,2,4); ... ; title('Masque image 25');
+
