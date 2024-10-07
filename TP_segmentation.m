a8e41b67c1043d9886e2c0b19b4e254839fc1333
Achille Jeanvoine <jeanvoine.achille@gmail.com>
2024-10-07 12:00:39 +0200

    avancée de 2 TP, couleurs ok

--- TP_segmentation.m
+++ TP_segmentation.m
@@ -34,28 +34,30 @@ figure;
 
 % Variables
 num = 1;
-K = 10;
+K = 100;
 Seuil = 10;
 m = 10;
 
 % Correction de K
 K = ceil(sqrt(K))^2;
-tailleCarre = sqrt(K);
 
 % Choix de l'image
 image = im(:,:,:,num);
 
-sizeX = size(image,2);
-sizeY = size(image,1);
+sizeY = size(image, 1);
+sizeX = size(image, 2);
 N =  sizeX * sizeY;
 S = sqrt(N/K); % dimension d'un côté d'une région à l'itération 0
 
-% Placement des germes
-posX = zeros(tailleCarre, tailleCarre);
-posY = zeros(tailleCarre, tailleCarre);
+grille_superpixels_x = floor (sizeX / S);
+grille_superpixels_y = floor (sizeY / S);
 
-for i = 0:tailleCarre-1
-    for j = 0:tailleCarre-1
+% Placement des germes
+posX = zeros(grille_superpixels_x, grille_superpixels_y);
+posY = zeros(grille_superpixels_x, grille_superpixels_y);
+
+for i = 0:grille_superpixels_x-1
+    for j = 0:grille_superpixels_y-1
         posX(i+1,j+1) = S/2 + S*i;
         posY(i+1,j+1) = S/2 + S*j;
     end
@@ -65,82 +67,144 @@ end
 
 % Selon Y
 distToTop = posY(1,1);
-distToBottom = sizeY - posY(tailleCarre, tailleCarre);
+distToBottom = sizeY - posY(grille_superpixels_x, grille_superpixels_y);
 offsetY = (distToBottom + distToTop)/2 - distToTop;
 posY = posY + offsetY;
 
 % Selon X
 distToLeft = posX(1,1);
-distToRight = sizeX - posX(tailleCarre, tailleCarre);
+distToRight = sizeX - posX(grille_superpixels_x, grille_superpixels_y);
 offsetX = (distToRight + distToLeft)/2 - distToLeft;
 posX = posX + offsetX;
 
+posY = round (posY);
+posX = round (posX);
+
+
 imshow(im(:,:,:,num)); title('Image');
+
 hold on;
+
 scatter(posX, posY, 'r+', 'LineWidth', 2);
 
 % A enlever pour faire la suite
-return
+% return
 
 %% Deuxième partie
+
+nb_iter_max = 1;
+nb_current_iter = 0;
 
 % Algorithme itératif
 while 1
 
     % Calcul des superpixels
+    etiquette = zeros(sizeX,sizeY,2);
 
     % On boucle sur chaque pixel
-    for px_i = 1:sizeX
-        for px_j = 1:sizeY
+    for px_x = 1:sizeX
+        for px_y = 1:sizeY
 
             % Recherche du meilleur germe pour chaque pixel
             min_d = realmax;
-            best_germe_i = 0;
-            best_germe_j = 0;
 
             % On boucle sur chaque germe
-            for germe_i = 1:tailleCarre
-                for germe_j = 1:tailleCarre
+            for germe_i = 1:grille_superpixels_x
+                for germe_j = 1:grille_superpixels_y
                     % Distance du pixel au centre
-                    dx = abs(px_i - posX(germe_i,germe_j));
-                    dy = abs(px_y - posY(germe_i,germe_j));
-
+                    dx = abs(px_x - posX(germe_i, germe_j));
+                    dy = abs(px_y - posY(germe_i, germe_j));
+                    % 
+                    % if ((dx > S || dy > S) && min_d ~= realmax)
+                    %     continue
+                    % end
                     if (dx > S || dy > S)
-                        break
+                        continue
                     end
                     dxy = sqrt(dx^2 + dy^2);
-                    pixel_RGB = image(px_i, px_j, :);
-                    germe_RGB = image(germe_i, germe_j,:);
+                    pixel_RGB = image(px_y, px_x, :);
+                    germe_RGB = image(posY(germe_i, germe_j), posX(germe_i, germe_j), :);
                     drgb = sum(pixel_RGB - germe_RGB);
 
-                    D = drbg + (m/S)*dxy;
+                    D = drgb + (m/S)*dxy;
                     
                     % Condition de nouveau meilleur germe
                     if min_d > D
                         min_d = D;
-                        best_germe_i = germe_i;
-                        best_germe_j = germe_j;                       
+
+                        % On choisit id_germe = (x-1) * tailleCarre + y
+                        %etiquette(px_x, px_y) = (germe_i-1) * grille_superpixels_y + germe_j;
+                        etiquette(px_x, px_y,1) = germe_i;
+                        etiquette(px_x, px_y,2) = germe_j;
                     end
                     
                 end
             end
-        end
 
-    end
-    
-    for px_i = (max(0, posX(i,j)-S)) : (min(sizeX, posX(i,j)+S))
-        for px_j = (max(0, posY(i,j)-S)) : (min(sizeY, posY(i,j)+S))
-                    
+       
         end
     end
+
+    % color = uint8(rand(grille_superpixels_x, grille_superpixels_y, 3) * 255);
+    color = zeros (grille_superpixels_x, grille_superpixels_y, 3);
+    for germe_i = 1:grille_superpixels_x
+        for germe_j = 1:grille_superpixels_y
+            color (germe_i, germe_j, :) = image (posY(germe_i, germe_j), posX(germe_i, germe_j), :);
+        end
+    end
+
+    % g_i = floor((etiquette-1)/grille_superpixels_y) + 1;
+    % g_j = mod(etiquette,grille_superpixels_y) + 1;
+    image_etiquette = zeros (sizeY, sizeX, 3);
+    for x = 1:sizeX
+        for y = 1:sizeY
+           image_etiquette(y, x, :) = color(etiquette(x,y,1), etiquette(x,y,2), :);
+        end
+    end
+    image_etiquette = uint8(image_etiquette);
+    
+
+    alpha = 1;
+    image_3 = alpha * image_etiquette + (1-alpha) * image;
+    imshow(image_3)
+
+    scatter(posX, posY, 'r+', 'LineWidth', 2);
 
     % Mise à jour des centres (moyennes des attributs)
+
+    % Obj : changer posX et posY
+
+    % pour chaque germe, on récupère les pixels attribués
+    positionsPixelsGermes = zeros(sizeX, sizeY, grille_superpixels_x, grille_superpixels_y);
+
+    for germe_i = 1:grille_superpixels_x
+        for germe_j = 1:grille_superpixels_y
+            %islinkedToCurrentGerm = find(etiquette(:,:,1) == germe_i) .* find(etiquette(:,:,2) == germe_j);
+        end
+    end
+
+
+    % maj = zeros (grille_superpixels_x, grille_superpixels_y, 1, 1, 1);    
+    % for px_x = 1:sizeX
+    %     for px_y = 1:sizeY
+    %         gi, gj = etiquette(px_x, px_y, :);
+    %         old_x, old_y, old_n = maj (gi, gj, :, :, :);
+    %         maj (gi, gj, 1, 1, 1) = maj (gi, gj, old_x + px_x, old_y + px_y, old_n + 1);
+    %     end
+    % end
+
     
     % Calcul de E (L1 entre les anciens et nouveaux Ck)
-    if E<Seuil
+    %if E<Seuil
+     %   break
+    %end
+
+    if nb_current_iter >= nb_iter_max
         break
     end
 
+    nb_current_iter = nb_current_iter + 1;
+    break
 end
 
 
