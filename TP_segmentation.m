6f3bfeb02fff4637663f27738f2aa4ba6a6c0b3c
Guilhem Mangenot <guilhem.mangenot@gmail.com>
2024-10-07 15:36:26 +0200

    une seule liste pour les germes

--- TP_segmentation.m
+++ TP_segmentation.m
@@ -1,7 +1,7 @@
 clear;
 close all;
-% Nombre d'images utilisees
-nb_images = 36; 
+
+nb_images = 36;     % Nombre d'images utilisees
 
 % chargement des images
 for i = 1:nb_images
@@ -23,162 +23,114 @@ figure;
 %subplot(2,2,3); imshow(im(:,:,:,17)); title('Image 17');
 %subplot(2,2,4); imshow(im(:,:,:,25)); title('Image 25');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% A COMPLETER                                             %
-% Calculs des superpixels                                 % 
-% Conseil : afficher les germes + les régions             %
-% à chaque étape / à chaque itération                     %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% VARIABLES
+K = 101;      % Nb de superpixels
+num = 1;      % N° de l'image traitée
+seuil = 10;   % Seuil pour condition d'arrêt
+m = 10;       % Poids de la distance géométrique (contre la distance colorimétrique)
 
-% ........................................................%
+%% INITIALISATION
 
-% Variables
-num = 1;
-K = 100;
-Seuil = 10;
-m = 10;
+% Image originale
+image = im(:, :, :, num);
+nb_px_x = size(image, 2);   % Nb de pixels sur la largeur
+nb_px_y = size(image, 1);   % Nb de pixels sur la hauteur
+N =  nb_px_x * nb_px_y;     % Nb de pixels
 
-% Correction de K
-K = ceil(sqrt(K))^2;
-
-% Choix de l'image
-image = im(:,:,:,num);
-
-sizeY = size(image, 1);
-sizeX = size(image, 2);
-N =  sizeX * sizeY;
-S = sqrt(N/K); % dimension d'un côté d'une région à l'itération 0
-
-grille_superpixels_x = floor (sizeX / S);
-grille_superpixels_y = floor (sizeY / S);
+% Superpixels
+S = sqrt(N/K);                            % A l'itération 0, un superpixel à une taille S*S
+nb_superpixels_x = ceil (nb_px_x / S);   % Nb de superpixels sur la largeur
+nb_superpixels_y = ceil (nb_px_y / S);   % Nb de superpixels sur la hauteur
 
 % Placement des germes
-posX = zeros(grille_superpixels_x, grille_superpixels_y);
-posY = zeros(grille_superpixels_x, grille_superpixels_y);
-
-for i = 0:grille_superpixels_x-1
-    for j = 0:grille_superpixels_y-1
-        posX(i+1,j+1) = S/2 + S*i;
-        posY(i+1,j+1) = S/2 + S*j;
+germes = zeros (nb_superpixels_x, nb_superpixels_y, 2);
+for i = 1:nb_superpixels_x
+    for j = 1:nb_superpixels_y
+        germes(i, j, 1) = (nb_px_x - S * (nb_superpixels_x-1))/2 + S*(i-1); % x
+        germes(i, j, 2) = (nb_px_y - S * (nb_superpixels_y-1))/2 + S*(j-1); % y
     end
 end
+germes = reshape(germes, [], 2); % Flattening
 
-% Recentrage des germes
-
-% Selon Y
-distToTop = posY(1,1);
-distToBottom = sizeY - posY(grille_superpixels_x, grille_superpixels_y);
-offsetY = (distToBottom + distToTop)/2 - distToTop;
-posY = posY + offsetY;
-
-% Selon X
-distToLeft = posX(1,1);
-distToRight = sizeX - posX(grille_superpixels_x, grille_superpixels_y);
-offsetX = (distToRight + distToLeft)/2 - distToLeft;
-posX = posX + offsetX;
-
-posY = round (posY);
-posX = round (posX);
-
-
+% Plot
 imshow(im(:,:,:,num)); title('Image');
-
 hold on;
+scatter(germes(:, 1), germes(:, 2), 'r+', 'LineWidth', 2);
 
-scatter(posX, posY, 'r+', 'LineWidth', 2);
-
-% A enlever pour faire la suite
-% return
-
-%% Deuxième partie
+%% ITÉRATIONS
 
 nb_iter_max = 1;
 nb_current_iter = 0;
 
-% Algorithme itératif
 while 1
-
-    % Calcul des superpixels
-    etiquette = zeros(sizeX,sizeY,2);
+    %% Calcul des superpixels
+    etiquettes = zeros(nb_px_y, nb_px_x, 1); % Appartenances pixel/germe
 
     % On boucle sur chaque pixel
-    for px_x = 1:sizeX
-        for px_y = 1:sizeY
+    for px_x = 1:nb_px_x
+        for px_y = 1:nb_px_y
 
             % Recherche du meilleur germe pour chaque pixel
             min_d = realmax;
+            for germe = 1:length(germes)
+                % Distance géométrique pixel-germe selon X et Y
+                dx = abs(px_x - germes(germe, 1));
+                dy = abs(px_y - germes(germe, 2));
 
-            % On boucle sur chaque germe
-            for germe_i = 1:grille_superpixels_x
-                for germe_j = 1:grille_superpixels_y
-                    % Distance du pixel au centre
-                    dx = abs(px_x - posX(germe_i, germe_j));
-                    dy = abs(px_y - posY(germe_i, germe_j));
-                    % 
-                    % if ((dx > S || dy > S) && min_d ~= realmax)
-                    %     continue
-                    % end
-                    if (dx > S || dy > S)
-                        continue
-                    end
-                    dxy = sqrt(dx^2 + dy^2);
-                    pixel_RGB = image(px_y, px_x, :);
-                    germe_RGB = image(posY(germe_i, germe_j), posX(germe_i, germe_j), :);
-                    drgb = sum(pixel_RGB - germe_RGB);
-
-                    D = drgb + (m/S)*dxy;
-                    
-                    % Condition de nouveau meilleur germe
-                    if min_d > D
-                        min_d = D;
-
-                        % On choisit id_germe = (x-1) * tailleCarre + y
-                        %etiquette(px_x, px_y) = (germe_i-1) * grille_superpixels_y + germe_j;
-                        etiquette(px_x, px_y,1) = germe_i;
-                        etiquette(px_x, px_y,2) = germe_j;
-                    end
-                    
+                % Le choix du germe doit être dans un voisinage 2S*2S
+                if (dx > S || dy > S)
+                    continue
                 end
+
+                % Distance géométrique
+                d_xy = sqrt(dx^2 + dy^2);
+                % Distance colorimétrique (RGB)
+                pixel_rgb = image(px_y, px_x, :);   
+                germe_rgb = image(round(germes(germe, 2)), round(germes(germe, 1)), :);
+                d_rgb = sum(pixel_rgb - germe_rgb);
+                % Distance pondérée
+                d_s = d_rgb + (m/S)*d_xy;
+                
+                % Condition de nouveau meilleur germe
+                if min_d > d_s
+                    min_d = d_s;
+                    etiquettes(px_y, px_x, 1) = germe;
+                end
+
             end
 
-       
         end
     end
 
-    % color = uint8(rand(grille_superpixels_x, grille_superpixels_y, 3) * 255);
-    color = zeros (grille_superpixels_x, grille_superpixels_y, 3);
-    for germe_i = 1:grille_superpixels_x
-        for germe_j = 1:grille_superpixels_y
-            color (germe_i, germe_j, :) = image (posY(germe_i, germe_j), posX(germe_i, germe_j), :);
+    %% Plot
+    % Couleur d'un superpixel = couleur du pixel le + proche du germe
+    color = zeros (length(germes), 3);
+    for germe = 1:length(germes)
+        color (germe, :) = image (round(germes(germe, 2)), round(germes(germe, 1)), :);
+    end
+    % color = uint8(rand(length(germes), 3) * 255); % couleurs aléatoires
+    % Couleur d'un pixel = couleur du superpixel auquel il appartient
+    image_etiquettes = zeros (nb_px_y, nb_px_x, 3);
+    for x = 1:nb_px_x
+        for y = 1:nb_px_y
+           image_etiquettes(y, x, :) = color(etiquettes(y,x,1), :);
         end
     end
-
-    % g_i = floor((etiquette-1)/grille_superpixels_y) + 1;
-    % g_j = mod(etiquette,grille_superpixels_y) + 1;
-    image_etiquette = zeros (sizeY, sizeX, 3);
-    for x = 1:sizeX
-        for y = 1:sizeY
-           image_etiquette(y, x, :) = color(etiquette(x,y,1), etiquette(x,y,2), :);
-        end
-    end
-    image_etiquette = uint8(image_etiquette);
-    
-
+    image_etiquettes = uint8(image_etiquettes);
+    % Superpixels en transparence
     alpha = 1;
-    image_3 = alpha * image_etiquette + (1-alpha) * image;
-    imshow(image_3)
-
-    scatter(posX, posY, 'r+', 'LineWidth', 2);
+    imshow(alpha * image_etiquettes + (1-alpha) * image)
+    scatter(germes(:, 1), germes(:, 2), 'r+', 'LineWidth', 2);
 
     % Mise à jour des centres (moyennes des attributs)
 
     % Obj : changer posX et posY
 
     % pour chaque germe, on récupère les pixels attribués
-    positionsPixelsGermes = zeros(sizeX, sizeY, grille_superpixels_x, grille_superpixels_y);
+    positionsPixelsGermes = zeros(nb_px_x, nb_px_y, nb_superpixels_x, nb_superpixels_y);
 
-    for germe_i = 1:grille_superpixels_x
-        for germe_j = 1:grille_superpixels_y
+    for germe_i = 1:nb_superpixels_x
+        for germe_j = 1:nb_superpixels_y
             %islinkedToCurrentGerm = find(etiquette(:,:,1) == germe_i) .* find(etiquette(:,:,2) == germe_j);
         end
     end
